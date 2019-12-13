978b2da591fe9b1614fadefeec47dbed3adbe988
Ondrej Kalman <ondrej.kalman@thefuntasty.com>
2019-12-13 14:16:24 +0100

    Feat: release the app for multiple groups

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
-appcenter distribute release --token "$INPUT_TOKEN" --app "$INPUT_APPNAME" --group "$INPUT_GROUP" --file "$INPUT_FILE" --release-notes "$INPUT_RELEASENOTES" --debug
+isFirst=true
+releaseId=""
+export IFS=";"
+for group in $INPUT_GROUP; do
+    if ${isFirst} ; then
+        isFirst=false
+        appcenter distribute release --token "$INPUT_TOKEN" --app "$INPUT_APPNAME" --group $group --file "$INPUT_FILE" --release-notes "$INPUT_RELEASENOTES" --silent
+        releaseId=$(appcenter distribute releases list --token "$INPUT_TOKEN"  --app "$INPUT_APPNAME" | grep ID | head -1 | tr -s ' ' | cut -f2 -d ' ')
+    else
+        appcenter distribute releases add-destination --token "$INPUT_TOKEN" -d $group -t group -r $releaseId -s --app "$INPUT_APPNAME"
+    fi
+done
 
